63fa7ba55d05f245379173bf7fce62fdf2f25128
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-09-02 11:28:48 +0100

    client - bug issue with DOM, temporarily taking it down

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import HomePage from './pages/homePage/HomePage';
 
 const App = () => {
   return(
-    <>
-      <Router>
-        <Route exact path='/' component={HomePage} />
-      </Router>
-    </>
+    // <>
+    //   <Router>
+    //     <Route path='/' component={HomePage} />
+    //   </Router>
+    // </>
+    <HomePage />
   )
 }
 
