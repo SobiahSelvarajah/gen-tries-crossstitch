0b9c34906cbac55b9a22a834ed2c413010744542
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-09-12 22:59:12 +0100

    client - SinglePost.scss updated

--- client/src/components/homePagePost/HomePagePost.jsx
+++ client/src/components/homePagePost/HomePagePost.jsx
@@ -3,13 +3,13 @@ import crossStitch from '../../assets/cross-stitch.jpg';
 
 export default function HomePagePost() {
   return (
-    <section className='homePagePost__container'>
+    <article className='homePagePost__container'>
         <img
             className='homePagePost__image'
             src={crossStitch}
             alt='postImage' 
         />
-        <div className='homePagePost__info'>
+        <section className='homePagePost__info'>
             <div className='homePagePost__categories'>
                 <span className='homePagePost__category'>
                     Plants
@@ -25,13 +25,13 @@ export default function HomePagePost() {
             <span className='homePagePost__date'>
                 2 hours ago
             </span>
-        </div>
+        </section>
         <p className='homePagePost__description'>
             Starting a brand new day one stitch at a time.nhjdhksjsk fjhdfjskb sjhjkdf fskhfsjfd fjdhfkjs 
             fdsjkhfjs fjkhdsj fsjhnfkjs jhsdkjf fkjsk fljkvj fkdjls asljnc jhfkdjf 
             fsdkfjhsdkfsjd fkjkjfh sjkf fkjnf df sdf g h gd fs jhfkdjfnfjjd fdjkhgjkdf gjhdfn
             ldjfgkfdh fdjhkdj jkdfj jkdgjkfd khkjssds fksjdhks ffjsk 
         </p>
-    </section>
+    </article>
   )
 }
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -13,34 +13,36 @@ export default function SinglePost() {
                 src={crossStitch}
                 alt='blogPostImage'
             />
-            <section className='singlePost__headingContainer'>
-                <h1 className='singlePost__heading'>
-                    Kittens
-                </h1>
-                <div className='singlePost__editIcons'>
-                    <img 
-                        className='singlePost__editIcon'
-                        src={editIcon}
-                        alt='editIcon'
-                    />
-                    <img 
-                        className='singlePost__deleteIcon'
-                        src={deleteIcon}
-                        alt='deleteIcon'
-                    />
-                </div>
-            </section>
-            <section className='singlePost__info'>
-                <h4 className='singlePost__author'>
-                    Genevieve
-                </h4>
-                <span className='singlePost__date'>
-                    2 hours ago
-                </span>
-            </section>
-            <p className='singlePost__description'>
-                jfkdj dsjhfdsjk dkjshjksh
-            </p>
+            <article className='singlePost__textWrap'>
+                <section className='singlePost__headingContainer'>
+                    <h1 className='singlePost__heading'>
+                        Kittens
+                    </h1>
+                    <div className='singlePost__editIcons'>
+                        <img 
+                            className='singlePost__editIcon'
+                            src={editIcon}
+                            alt='editIcon'
+                        />
+                        <img 
+                            className='singlePost__deleteIcon'
+                            src={deleteIcon}
+                            alt='deleteIcon'
+                        />
+                    </div>
+                </section>
+                <section className='singlePost__info'>
+                    <h4 className='singlePost__author'>
+                        Genevieve
+                    </h4>
+                    <span className='singlePost__date'>
+                        2 hours ago
+                    </span>
+                </section>
+                <p className='singlePost__description'>
+                    jfkdj dsjhfdsjk dkjshjksh
+                </p>
+            </article>
         </main>
     )
 }
