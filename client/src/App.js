58af5571b2a565d22637dbf77a27f271986c7c16
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-09-17 00:01:20 +0100

    client - login component added (responsive)

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/navBar/NavBar';
 // import HomePage from './pages/homePage/HomePage';
 // import SinglePostPage from './pages/singlePostPage/SinglePostPage';
 // import PublishPost from './pages/publishPost/PublishPost';
-import AccountSettings from './pages/accountSetting/AccountSettings';
+// import AccountSettings from './pages/accountSetting/AccountSettings';
 import Login from './pages/login/Login';
 
 
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
 
+import './Login.scss';
+
 export default function Login() {
   return (
     <section className='login__container'>
-        <h1 className='login__heading'>
-            Login
-        </h1>
-        <form className='login__form'>
-            <label className='login__userInfo'>
-                Email
-            </label>
-            <input
-                className='login__userInput'
-                type='email'
-                placeholder='Enter your email...'
-            />
-            <label className='login__userInfo'>
-                Password
-            </label>
-            <input
-                className='login__userInput'
-                type='password'
-                placeholder='Enter your password...'
-            />
-            <button className='login__loginButton'>
-                LOGIN
-            </button>
-        </form>
-        <button className='login__registerButton'>
-            Register
-        </button>        
+        <div className='login__containerWrapper'>
+            <h1 className='login__heading'>
+                Login
+            </h1>
+            <form className='login__form'>
+                <label className='login__userInfo'>
+                    Email
+                </label>
+                <input
+                    className='login__userInput'
+                    type='email'
+                    placeholder='Enter your email...'
+                />
+                <label className='login__userInfo'>
+                    Password
+                </label>
+                <input
+                    className='login__userInput'
+                    type='password'
+                    placeholder='Enter your password...'
+                />
+                <button className='login__loginButton'>
+                    LOGIN
+                </button>
+            </form>
+            <button className='login__registerButton'>
+                Register
+            </button>  
+        </div>      
     </section>
   )
 }
