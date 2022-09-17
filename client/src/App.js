5f5881a0fc2ff2d53ed1c09736bd951c415d759a
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-09-17 20:57:01 +0100

    client - react-router-dom added to app.js and NavLinks.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-// import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link, Routes} from 'react-router-dom';
 import NavBar from './components/navBar/NavBar';
 import HomePage from './pages/homePage/HomePage';
 import SinglePostPage from './pages/singlePostPage/SinglePostPage';
 import PublishPost from './pages/publishPost/PublishPost';
-import AccountSettings from './pages/accountSetting/AccountSettings';
+import AccountSettings from './pages/accountSettings/AccountSettings';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 
-
-const App = () => {
+function App() {
+  const user = false;
   return(
-    <>
-      <NavBar />
-      <HomePage />
-      <SinglePostPage />
-      <PublishPost />
-      <AccountSettings />
-      <Login />
-      <Register />
-    </>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route exact path='/' element={<HomePage/>} />
+        <Route 
+          path='/register' 
+          element={user ? <HomePage/> : <Register/>} 
+        />      
+        <Route 
+          path='/login' 
+          element={user ? <HomePage/> : <Login/>} 
+        />   
+        <Route 
+          path='/publish' 
+          element={user ? <PublishPost/> : <Register/>} 
+        />
+        <Route 
+          path='/settings' 
+          element={user ? <AccountSettings/> : <Register/>} 
+        />                         
+        <Route 
+          path='/post/:postId' 
+          element={user ? <SinglePostPage/> : <Register/>} 
+        />
+      </Routes>
+    </Router>
   )
 }
 
--- client/src/components/navLinks/NavLinks.jsx
+++ client/src/components/navLinks/NavLinks.jsx
@@ -1,33 +1,35 @@
+import { Link } from 'react-router-dom';
 import './NavLinks.scss';
 
 export default function NavLinks() {
+    const user = true;
     return(
-        <div className='navLinks__container'>
-            <a
-                className='navLinks__element' 
-                href='/'>
-                HOME
-            </a>
-            <a 
-                className='navLinks__element'
-                href='/about'>
-                ABOUT
-            </a>
-            <a 
-                className='navLinks__element'
-                href='/contact'>
-                CONTACT
-            </a>
-            <a 
-                className='navLinks__element'
-                href='/mystitches'>
-                MY STITCHES
-            </a>
-            <a 
-                className='navLinks__element'
-                href='/logout'>
-                LOGOUT
-            </a>
-        </div>
+        <ul className='navLinks__container'>
+            <li className='navLinks__element'>
+                <Link to='/' className='navLinks__link'>
+                    HOME
+                </Link>
+            </li>
+            <li className='navLinks__element'>
+                <Link to='/about' className='navLinks__link'>
+                    ABOUT
+                </Link>
+            </li>
+            <li className='navLinks__element'>
+                <Link to='/contact' className='navLinks__link'>
+                    CONTACT
+                </Link>
+            </li>
+            <li className='navLinks__element'>
+                <Link to='/myStitches' className='navLinks__link'>
+                    MY STITCHES
+                </Link>
+            </li>
+            <li className='navLinks__element'>
+                <Link to='/logout' className='navLinks__link'>
+                    LOGOUT
+                </Link>
+            </li>
+        </ul>
     )
 }
