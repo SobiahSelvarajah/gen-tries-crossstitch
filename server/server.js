175cc8d2a1d264e6765d1800e293cf0e341ede8a
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-09-19 22:19:37 +0100

    server - server.js set up

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+    useCreateIndex: true,
+    })
+    .then(console.log("Connected to mongo"))
+    .catch((err) => console.log(err));
 
 app.listen("8080", () => {
     console.log("Backend is running")
