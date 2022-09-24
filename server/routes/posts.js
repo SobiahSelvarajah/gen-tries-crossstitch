e443137064bc0e87e2fb01a2c37bd4cf989a0bec
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-09-24 23:55:47 +0100

    server - posts.js: get request for single post completed

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
 
+// Get request - get single post
+router.get("/:postID", async(req, res) => {
+    try {
+        const singlePostID = req.params.postID;
+        const singlePost = await Post.findById(singlePostID);
+        singlePost && res.status(200).json(singlePost);
+    }
+    catch(err) {
+        res.status(500).json("Oops, server error...will be fixed shortly");
+    }
+});
+
 // Post request - create new post
 router.post("/", async(req, res) => {
     const creatingNewPost = new Post(req.body);
