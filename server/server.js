11563bf9e8773db207ebe7b1a33a7e155328156f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-09-26 20:33:14 +0100

    server - image storage system added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
@@ -14,6 +15,21 @@ mongoose
     .connect(process.env.MONGO_URL)
     .then(console.log("Connected to mongo"))
     .catch((err) => console.log(err));
+
+const fileStorage = multer.diskStorage({
+    // callback func deals with errors
+    destination: (req, imageFile, callback) => {
+        callback(null, "images");
+    }, 
+    filename: (req, imageFile, callback) => {
+        callback(null, req.body.name);
+    },
+});
+
+const uploadfile = multer({storage: fileStorage});
+app.post("/server/upload", uploadfile.single("imageFile"), (req, res) => {
+    res.status(200).json("This image file has been uploaded.");
+});
 
 app.use("/server/auth", authRoute);
 app.use("/server/users", usersRoute);
