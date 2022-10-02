3a563780812b0a5bf5c3899397e463c7f9a9fcca
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-10-02 22:32:28 +0100

    client - homePagePost: Link to single post added

--- client/src/components/homePagePost/HomePagePost.jsx
+++ client/src/components/homePagePost/HomePagePost.jsx
@@ -1,35 +1,38 @@
+import { Link } from 'react-router-dom';
 import './HomePagePost.scss';
 
 const HomePagePost = ({post}) => {
   return (
-    <article className='homePagePost__container'>
-        {post.postImage && (
-            <img
-                className='homePagePost__postImage'
-                src={post.postImage}
-                alt='postImage' 
-            />
-        )}
-        <section className='homePagePost__info'>
-            <div className='homePagePost__categories'>
-                {post.categories.map((category) => (
-                    <h5 className='homePagePost__category'>
-                        {category.name}
-                    </h5>                    
-                ))}
-            </div>
-            <h2 className='homePagePost__postTitle'>
-                {post.postTitle}
-            </h2>
-            <hr></hr>
-            <h5 className='homePagePost__timestamps'>
-                {new Date(post.createdAt).toDateString()}
-            </h5>
-        </section>
-        <p className='homePagePost__description'>
-            {post.description}
-        </p>
-    </article>
+    <Link to={`/post/${post._id}`} style={{textDecoration:'none'}}>
+        <article className='homePagePost__container'>
+            {post.postImage && (
+                <img
+                    className='homePagePost__postImage'
+                    src={post.postImage}
+                    alt='postImage' 
+                />
+            )}
+            <section className='homePagePost__info'>
+                <div className='homePagePost__categories'>
+                    {post.categories.map((category) => (
+                        <h5 className='homePagePost__category'>
+                            {category.name}
+                        </h5>                    
+                    ))}
+                </div>
+                <h2 className='homePagePost__postTitle'>
+                    {post.postTitle}
+                </h2>
+                <hr></hr>
+                <h5 className='homePagePost__timestamps'>
+                    {new Date(post.createdAt).toDateString()}
+                </h5>
+            </section>
+            <p className='homePagePost__description'>
+                {post.description}
+            </p>
+        </article>
+    </Link>
   )
 }
 
