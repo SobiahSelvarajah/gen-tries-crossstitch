db953e27200e8215403bfdb5279b25c70d4a5553
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-10-04 21:43:48 +0100

    client - deletePostIcon: new component added

---
+++ client/src/components/deletePostIcon/DeletePostIcon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import deleteIcon from '../../assets/icons/delete.svg'; 
+import './DeletePostIcon.scss';
+
+export default function DeletePostIcon() {
+    return(
+        <img 
+            className='deletePostIcon__image'
+            src={deleteIcon}
+            alt='deleteIcon'
+        /> 
+    )
+}
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,48 +1,46 @@
 import React from 'react';
-import deleteIcon from '../../assets/icons/delete.svg'; 
+import DeletePostIcon from '../deletePostIcon/DeletePostIcon';
 import EditPostIcon from '../editPostIcon/EditPostIcon';
 
 import './SinglePost.scss';
 
 const SinglePost = ({post}) => {
     return(
-        <main className='singlePost__container'>
-            <div className='singlePost__imageContainer'>
-                {post.postImage && (
-                    <img 
-                        className='singlePost__postImage'
-                        src={post.postImage}
-                        alt='blogPostImage'
-                    />                    
-                )}
-            </div>
-            <article className='singlePost__textWrap'>
-                <section className='singlePost__titleContainer'>
-                    <h1 className='singlePost__postTitle'>
-                        {post.postTitle}
-                    </h1>
-                    <div className='singlePost__editIcons'>
-                        <EditPostIcon />
+        <>
+            <main className='singlePost__container'>
+                <div className='singlePost__imageContainer'>
+                    {post.postImage && (
                         <img 
-                            className='singlePost__deleteIcon'
-                            src={deleteIcon}
-                            alt='deleteIcon'
-                        /> 
-                    </div>
-                </section>
-                <section className='singlePost__info'>
-                    <h4 className='singlePost__username'>
-                        {post.username}
-                    </h4>
-                    <span className='singlePost__timestamps'>
-                        {new Date(post.createdAt).toDateString()}
-                    </span>
-                </section>
-                <p className='singlePost__description'>
-                    {post.description}
-                </p>
-            </article>
-        </main>
+                            className='singlePost__postImage'
+                            src={post.postImage}
+                            alt='blogPostImage'
+                        />                    
+                    )}
+                </div>
+                <article className='singlePost__textWrap'>
+                    <section className='singlePost__titleContainer'>
+                        <h1 className='singlePost__postTitle'>
+                            {post.postTitle}
+                        </h1>
+                        <div className='singlePost__editIcons'>
+                            <EditPostIcon />
+                            <DeletePostIcon />
+                        </div>
+                    </section>
+                    <section className='singlePost__info'>
+                        <h4 className='singlePost__username'>
+                            {post.username}
+                        </h4>
+                        <span className='singlePost__timestamps'>
+                            {new Date(post.createdAt).toDateString()}
+                        </span>
+                    </section>
+                    <p className='singlePost__description'>
+                        {post.description}
+                    </p>
+                </article>
+            </main>
+        </>
     )
 }
 
