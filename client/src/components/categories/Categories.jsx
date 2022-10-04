174f610c2fa97bca6876257d0250f2fe688e5485
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-10-04 12:58:24 +0100

    client - Sidebar: useFetchCategories added to Sidebar

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useFetchCategories } from '../../hooks/useFetchCategories';
 
 const Categories = () => {
+    const { categories, error } = useFetchCategories("/categories/", []);
+    
     return(
         <ul className='categories__list'>
-            <li className='categories__listItem'>
-                Plants
-            </li>
+            {error && <p>{error}</p>}
+            {categories.map((category) => (
+                <li className='categories__listItem'>
+                    {category.name}
+                </li>                
+            ))}
         </ul>
     )
 }
 
-export default Categories
+export default Categories;
