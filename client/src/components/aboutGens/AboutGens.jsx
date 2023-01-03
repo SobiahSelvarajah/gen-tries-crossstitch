dec837a1eb8456db7077c0a1b6bc92c4eea88466
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-03 12:26:31 +0000

    client - aboutGensText: component added

--- client/src/components/aboutGens/AboutGens.jsx
+++ client/src/components/aboutGens/AboutGens.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import AboutGensPic from "../aboutGensPic/AboutGensPic";
+import AboutGensText from "../aboutGensText/AboutGensText";
 import './AboutGens.scss';
 
 export default function AboutGens() {
     return(
         <>
             <AboutGensPic />
-            {/* <AboutGensText/> */}
+            <AboutGensText />
         </>
     )
 }
---
+++ client/src/components/aboutGensText/AboutGensText.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './AboutGensText.scss';
+
+export default function AboutGensText() {
+    return(
+        <section className="aboutGensText__container">
+            <h2 className="aboutGensText__heading">
+                Genevive Williams
+            </h2>
+            <p className="aboutGensText__description">
+                Roses are red, violets are blue, butterflies are pretty and so are you.
+            </p>
+        </section>
+    )
+}
