812fbc215c3725a75642d3545dbf1f049b3c66bb
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-03 14:08:17 +0000

    client - aboutSobs: component added

---
+++ client/src/components/aboutSobs/AboutSobs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AboutSobs.scss";
+
+export default function AboutSobs() {
+    return(
+        <>
+            {/* <AboutSobsPic /> */}
+            {/* <AboutSobsText /> */}
+        </>
+    )
+}
