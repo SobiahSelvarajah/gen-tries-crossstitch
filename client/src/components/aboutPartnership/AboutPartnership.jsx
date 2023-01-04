053ee68c27b9e00d7c874656bcbb0d4b59e13b32
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-04 16:32:04 +0000

    client - aboutPartnershipText: component added

--- client/src/components/aboutPartnership/AboutPartnership.jsx
+++ client/src/components/aboutPartnership/AboutPartnership.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AboutPartnershipPic from "../aboutPartnershipPic/AboutPartnershipPic";
+import AboutPartnershipText from "../aboutPartnershipText/AboutPartnershipText";
 
 export default function AboutPartnership() {
     return(
         <>
             <AboutPartnershipPic />
-            {/* <AboutPartnershipText/> */}
+            <AboutPartnershipText />
         </>
     )
 }
---
+++ client/src/components/aboutPartnershipText/AboutPartnershipText.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AboutPartnershipText() {
+    return(
+        <section className="aboutPartnershipText__container">
+            <h2 className="aboutPartnershipText__heading">
+                Our Partnership
+            </h2>
+            <p className="aboutPartnershipText__description">
+                A united team is a successful team
+            </p>
+        </section>
+    )
+}
