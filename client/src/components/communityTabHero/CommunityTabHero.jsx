e0a6985db4c7ff7be1d1ea972b5f8db3bf5a278c
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-06 00:24:12 +0000

    client - communityTabHero: component added

---
+++ client/src/components/communityTabHero/CommunityTabHero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CommunityTabHero.scss";
+
+export default function CommunityTabHero() {
+    return(
+        <>
+            {/* <CommunityTabHeroText/> */}
+        </>
+    )
+}
