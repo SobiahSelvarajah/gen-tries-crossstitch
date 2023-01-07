27e0ee62077412851e6760eef4045da64865c002
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-07 22:06:11 +0000

    client - communityTabHero: component restructured

--- client/src/components/communityTabHero/CommunityTabHero.jsx
+++ client/src/components/communityTabHero/CommunityTabHero.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import CommunityTabHeroText from "../communityTabHeroText/CommunityTabHeroText";
+import heroImage from "../../assets/images/community-hero.jpg";
 import "./CommunityTabHero.scss";
 
 export default function CommunityTabHero() {
     return(
-        <>
-            <CommunityTabHeroText/>
-        </>
+        <div className="communityTabHero__container">
+            <img
+                className="communityTabHero__image"
+                src={heroImage}
+                alt="heroImage"
+            />
+            <section className="communityTabHero__textContainer">
+                <h2 className="communityTabHero__title">
+                    Our Stitching Community
+                </h2>
+                <h5 className="communityTabHero__description">
+                    This is a discussion zone, where you can disucss new topics, ask questions and share new tips and tricks.
+                </h5>
+            </section>            
+        </div>
+
     )
 }
