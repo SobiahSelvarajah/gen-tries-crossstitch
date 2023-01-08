e46414e2e2a8893f729d4073b921980e2d781f3c
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-08 23:44:21 +0000

    client - communityTopic: component added

---
+++ client/src/components/communityTopic/CommunityTopic.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./CommunityTopic.scss";
+
+export default function CommunityTopic() {
+    return(
+        <section className="communityTopic__container">
+            <h4 className="communityTopic__heading">
+                Loop stitch
+            </h4>
+            <h4 className="communityTopic__replies">
+                16 replies
+            </h4>
+            <h4 className="communityTopic__publishedDate">
+                16th Feb 2023
+            </h4>
+        </section>
+    )
+}
