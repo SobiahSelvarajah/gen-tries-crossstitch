8eed11137ef8151cd76b411731ff6af83a3f970c
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-10 22:53:21 +0000

    client - footer: component updated

--- client/src/components/footerLinks/FooterLinks.jsx
+++ client/src/components/footerLinks/FooterLinks.jsx
@@ -1,48 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./FooterLinks.scss";
 
 export default function FooterLinks() {
     return (
         <>
-            <div className="footerLinks__container">
+            <section className="footerLinks__container">
                 <h2 className="footerLinks__heading">
                     GTC
                 </h2>
                 <ul className="footerLinks__links">
-                    <li className="footerLinks__link">
-                        Home
+                    <li className="footerLinks__element">
+                        <Link to="/" className="footerLinks__link">Home</Link>
                     </li>
-                    <li className="footerLinks__link">
-                        About Us
+                    <li className="footerLinks__element">
+                        <Link to="/aboutus" className="footerLinks__link">About Us</Link>
                     </li>
                 </ul>
-            </div>    
-            <div className="footerLinks__container">
+            </section>    
+            <section className="footerLinks__container">
                 <h2 className="footerLinks__heading">
                     Our Community
                 </h2>
                 <ul className="footerLinks__links">
-                    <li className="footerLinks__link">
-                        Blogs
+                    <li className="footerLinks__element">
+                        <Link to="/blogs" className="footerLinks__link">Blogs</Link>
                     </li>
-                    <li className="footerLinks__link">
-                        Community Tab
+                    <li className="footerLinks__element">
+                        <Link to="/community" className="footerLinks__link">Community Tab</Link>
                     </li>
                 </ul>
-            </div>    
-            <div className="footerLinks__container">
+            </section>    
+            <section className="footerLinks__container">
                 <h2 className="footerLinks__heading">
                     Support
                 </h2>
                 <ul className="footerLinks__links">
-                    <li className="footerLinks__link">
-                        Terms and Rules
+                    <li className="footerLinks__element">
+                        <Link to="/terms" className="footerLinks__link">Terms and Rules</Link>
                     </li>
-                    <li className="footerLinks__link">
-                        Contact Us
+                    <li className="footerLinks__element">
+                        <Link to="/contactus" className="footerLinks__link">Contact Us</Link>
                     </li>
                 </ul>
-            </div>  
+            </section>  
         </>
     )
 }
--- client/src/components/navLinks/NavLinks.jsx
+++ client/src/components/navLinks/NavLinks.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavLinks() {
     const user = false;
     return(
-        <ul className='navLinks__container'>
-            <li className='navLinks__element'>
-                <Link to='/' className='navLinks__link'>HOME</Link>
+        <ul className="navLinks__container">
+            <li className="navLinks__element">
+                <Link to="/" className="navLinks__link">HOME</Link>
             </li>
-            <li className='navLinks__element'>
-                <Link to='/aboutus' className='navLinks__link'>ABOUT US</Link>
+            <li className="navLinks__element">
+                <Link to="/aboutus" className="navLinks__link">ABOUT US</Link>
             </li>
-            <li className='navLinks__element'>
-                <Link to='/stitches' className='navLinks__link'>STITCHES</Link>
+            <li className="navLinks__element">
+                <Link to="/stitches" className="navLinks__link">STITCHES</Link>
             </li>
-            <li className='navLinks__element'>
-                <Link to='/community' className='navLinks__link'>COMMUNITY TAB</Link>
+            <li className="navLinks__element">
+                <Link to="/community" className="navLinks__link">COMMUNITY TAB</Link>
             </li>
-            <li className='navLinks__element'>
-                <Link to='/write' className='navLinks__link'>WRITE</Link>
+            <li className="navLinks__element">
+                <Link to="/write" className="navLinks__link">WRITE</Link>
             </li>
-            <li className='navLinks__element'>
+            <li className="navLinks__element">
                 {user && 'LOGOUT'}
             </li>
         </ul>
